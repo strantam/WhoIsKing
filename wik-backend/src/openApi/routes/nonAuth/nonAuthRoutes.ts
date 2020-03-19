2a7b88ed5358f2f98e5c950f704656f1aa0e080d
strantam <stranigg.tamas@gmail.com>
2020-03-19 22:47:41 +0100

    get all cities done

--- wik-backend/src/openApi/routes/nonAuth/nonAuthRoutes.ts
+++ wik-backend/src/openApi/routes/nonAuth/nonAuthRoutes.ts
@@ -1,4 +1,7 @@
 import {getLogger} from "../../../log/logger";
+import {DB} from "../../../db";
+import {City} from "../../../db/DatabaseMapping";
+import {City as ApiCity} from "../../../openApi/model/city";
 
 
 const logger = getLogger(module.filename);
@@ -7,7 +10,10 @@ const express = require('express');
 
 const router = express.Router();
 
-router.post('/', async (req, res, next) => {
+router.get('/city', async (req, res, next) => {
+    const cities: Array<City> = (await DB.getDb().pool.query('SELECT * FROM "City" ORDER BY "name"')).rows;
+    const apiCities: Array<ApiCity> = cities;
+    res.json(apiCities);
 });
 
 
