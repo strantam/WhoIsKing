8a76f09b6f904a9c5049e3a4c38f842ba3c52f0b
strantam <stranigg.tamas@gmail.com>
2020-03-19 23:57:13 +0100

    delete user

--- wik-backend/src/openApi/routes/auth/authRoutes.ts
+++ wik-backend/src/openApi/routes/auth/authRoutes.ts
@@ -1,6 +1,8 @@
 import {getLogger} from "../../../log/logger";
 import {DB} from "../../../db";
 import {ErrorCode, ErrorObject, HttpStatus} from "../../../error/ErrorObject";
+import {User} from "../../../db/DatabaseMapping";
+import admin from "firebase-admin";
 
 
 const logger = getLogger(module.filename);
@@ -21,5 +23,24 @@ router.post('/city', async (req, res, next) => {
     }
 });
 
+router.delete('/user/me', async (req, res, next) => {
+    const dbClient = await DB.getDb().pool.connect();
+    try {
+        await dbClient.query('BEGIN');
+
+        const currentUser: User = (await DB.getDb().pool.query('DELETE FROM "User" WHERE "uid"=$1 RETURNING *', [res.locals.userId])).rows[0];
+        await admin.auth().deleteUser(currentUser.fireBaseId);
+
+        await dbClient.query('COMMIT');
+        res.json({});
+    } catch (err) {
+        await dbClient.query('ROLLBACK');
+
+        logger.error("Error deleting user with id: " + res.locals.userId + " " + JSON.stringify(err.message));
+        next(new ErrorObject(ErrorCode.DB_QUERY_ERROR, "Cannot delete user/me", HttpStatus.INTERNAL_SERVER));
+    } finally {
+        dbClient.release()
+    }
+});
 
 export const authRouter = router;
