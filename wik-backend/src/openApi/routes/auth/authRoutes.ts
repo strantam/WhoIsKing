64842d42146006f85ba762c37332aaac1ba752d2
strantam <stranigg.tamas@gmail.com>
2020-03-20 11:47:37 +0100

    post solution

--- wik-backend/src/openApi/routes/auth/authRoutes.ts
+++ wik-backend/src/openApi/routes/auth/authRoutes.ts
@@ -1,8 +1,9 @@
 import {getLogger} from "../../../log/logger";
 import {DB} from "../../../db";
 import {ErrorCode, ErrorObject, HttpStatus} from "../../../error/ErrorObject";
-import {User} from "../../../db/DatabaseMapping";
+import {Question, User} from "../../../db/DatabaseMapping";
 import admin from "firebase-admin";
+import * as uuid from 'uuid';
 
 
 const logger = getLogger(module.filename);
@@ -40,6 +41,45 @@ router.delete('/user/me', async (req, res, next) => {
         next(new ErrorObject(ErrorCode.DB_QUERY_ERROR, "Cannot delete user/me", HttpStatus.INTERNAL_SERVER));
     } finally {
         dbClient.release()
+    }
+});
+
+router.post('/game/:gameId', async (req, res, next) => {
+    const gameId = req.params.gameId;
+    const answer = req.body.answer;
+    try {
+        const question: Question = (await DB.getDb().pool.query('SELECT * FROM "Question" WHERE "uid" = $1 AND "openTime" < $2', [gameId, new Date()])).rows[0];
+        if (!question) {
+            throw new ErrorObject(ErrorCode.NO_OPEN_QUESTION, "Question not opened currently", HttpStatus.INTERNAL_SERVER);
+        }
+
+        let points: number = 0;
+        switch (question.questionType) {
+            case "ESTIMATION":
+                const answerNum = parseFloat(answer);
+                const goodAnswerNum = parseFloat(question.response);
+                console.log(answerNum, goodAnswerNum, ((Math.abs(goodAnswerNum) - Math.abs(answerNum - goodAnswerNum)) / Math.abs(goodAnswerNum)));
+                points = Math.round(((Math.abs(goodAnswerNum) - Math.abs(answerNum - goodAnswerNum)) / Math.abs(goodAnswerNum)) * question.points * 100) / 100;
+                break;
+            case "MULTIPLE_CHOICE":
+                if (question.response === answer) {
+                    points = question.points;
+                }
+                break;
+            default:
+                throw new ErrorObject(ErrorCode.UNKNOWN_QUESTION_TYPE, "Question type not known", HttpStatus.INTERNAL_SERVER);
+        }
+        const user: User = (await DB.getDb().pool.query('SELECT * FROM "User" WHERE "uid"=$1', [res.locals.userId])).rows[0];
+        await DB.getDb().pool.query('INSERT INTO "Solution" ("uid", "answer", "points", "userId", "questionId", "cityId") VALUES ($1, $2, $3, $4, $5, $6)', [uuid.v4(), answer, points, res.locals.userId, gameId, user.cityId]);
+
+        res.json({points: points});
+    } catch (err) {
+        logger.error("Error posting answer: " + JSON.stringify(err.message));
+        if (err.ownErrorObject) {
+            next(err);
+        } else {
+            next(new ErrorObject(ErrorCode.DB_QUERY_ERROR, "Error posting answer.", HttpStatus.INTERNAL_SERVER));
+        }
     }
 });
 
