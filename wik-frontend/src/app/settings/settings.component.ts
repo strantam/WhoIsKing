fe0c981a95bf3d6f4831d3ae96cda3d8e1105c26
strantam <stranigg.tamas@gmail.com>
2020-04-02 23:44:30 +0200

    change city from settings user reload fix

--- wik-frontend/src/app/settings/settings.component.ts
+++ wik-frontend/src/app/settings/settings.component.ts
@@ -3,6 +3,9 @@ import {AuthService} from "../authentication/auth.service";
 import {AddCityComponent} from "../addcity/add-city.component";
 import {MatDialog} from "@angular/material/dialog";
 import {HttpHandlerService} from "../http-service/http-handler.service";
+import {fetchUser} from "../reducers/user/user";
+import {Store} from "@ngrx/store";
+import {State} from "../reducers";
 
 @Component({
   selector: 'app-settings',
@@ -11,7 +14,10 @@ import {HttpHandlerService} from "../http-service/http-handler.service";
 })
 export class SettingsComponent implements OnInit {
 
-  constructor(public authService: AuthService, private dialog: MatDialog, private httpHandlerService: HttpHandlerService) {
+  constructor(public authService: AuthService,
+              private dialog: MatDialog,
+              private httpHandlerService: HttpHandlerService,
+              private store: Store<State>) {
   }
 
   ngOnInit() {
@@ -20,7 +26,7 @@ export class SettingsComponent implements OnInit {
   async openChangeCity() {
     const dialogRef = this.dialog.open(AddCityComponent);
     await dialogRef.afterClosed().toPromise();
-    location.reload();
+    this.store.dispatch(fetchUser());
   }
 
   async deleteUser() {
