63fc329f46522ea8e2a751468591756a8b653783
strantam <stranigg.tamas@gmail.com>
2020-05-20 20:32:09 +0200

    spinner timeout

--- wik-frontend/src/app/layout/spinner-wrapper/spinner.interceptor.ts
+++ wik-frontend/src/app/layout/spinner-wrapper/spinner.interceptor.ts
@@ -5,25 +5,32 @@ import {Observable} from "rxjs";
 import {State} from "../../reducers";
 import {addSpinner, removeSpinner} from "../../reducers/spinner/spinner";
 import {Injectable} from "@angular/core";
+import Timer = NodeJS.Timer;
 
 @Injectable()
 export class SpinnerInterceptor implements HttpInterceptor {
 
-  count = 0;
+  private count = 0;
+  private addSpinnerTimeout: Timer;
 
   constructor(private store: Store<State>) {
   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    this.store.dispatch(addSpinner());
-
     this.count++;
-
+    if (this.count === 1) {
+      this.addSpinnerTimeout = setTimeout(() => {
+        this.store.dispatch(addSpinner());
+      }, 600)
+    }
     return next.handle(req)
-
       .pipe(finalize(() => {
           this.count--;
-          if (this.count == 0) {
+          if (this.count === 0) {
+            if (this.addSpinnerTimeout) {
+              clearTimeout(this.addSpinnerTimeout);
+              this.addSpinnerTimeout = null;
+            }
             this.store.dispatch(removeSpinner());
           }
         })
